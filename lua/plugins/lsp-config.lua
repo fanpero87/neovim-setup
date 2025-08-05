df466e7183706eeed3b360bf01083734c41cad3d
Fabio Perez <fanpero@mac.home>
2025-08-05 00:03:28 -0400

    making changes to the plugins and adding which-key

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -19,16 +19,46 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		lazy = false,
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			{ "antosha417/nvim-lsp-file-operations", config = true },
+			{ "folke/neodev.nvim", opts = {} },
+		},
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({ capabilities = capabilities })
-			lspconfig.html.setup({ capabilities = capabilities })
-			lspconfig.cssls.setup({ capabilities = capabilities })
-			lspconfig.tailwindcss.setup({ capabilities = capabilities })
-			lspconfig.ts_ls.setup({ capabilities = capabilities })
-			lspconfig.intelephense.setup({ capabilities = capabilities })
+			vim.lsp.config("*", {
+				capabilities = capabilities,
+			})
+
+			vim.lsp.enable("bashls")
+			vim.lsp.enable("cssls")
+			vim.lsp.enable("html")
+			vim.lsp.enable("intelephense")
+			vim.lsp.enable("jsonls")
+			vim.lsp.enable("nginx_language_server")
+			vim.lsp.enable("tailwindcss")
+			vim.lsp.enable("ls_ls")
+
+			vim.diagnostic.config({
+				virtual_text = true, -- Enable virtual text for diagnostics
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = "",
+						[vim.diagnostic.severity.WARN] = "",
+						[vim.diagnostic.severity.INFO] = "",
+						[vim.diagnostic.severity.HINT] = "󰌵",
+					},
+				},
+				update_in_insert = false, -- Don't update diagnostics in insert mode
+				severity_sort = true, -- Sort diagnostics by severity
+				underline = { severity = vim.diagnostic.severity.ERROR },
+				float = {
+					border = "rounded",
+					source = true,
+				},
+			})
 
 			vim.keymap.set("n", "<Leader>d", vim.diagnostic.open_float, {}) -- show diagnostics for line
 			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {}) -- jump to previous diagnostic in buffer
@@ -37,14 +67,7 @@ return {
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {}) -- see available code actions
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {}) -- smart rename
-
-			vim.diagnostic.config({
-				virtual_text = false,
-				float = {
-					source = true,
-				},
-			})
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {}) -- smart rename
 		end,
 	},
 }
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,8 +7,35 @@ return {
 		"nvim-tree/nvim-web-devicons", -- vs-code like icons
 	},
 	lazy = false, -- neo-tree will lazily load itself
-	config = function()
-		vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left<CR>", {})
-		vim.keymap.set("n", "<leader>w", ":Neotree filesystem close<CR>", {})
-	end,
+	opts = {
+		enable_git_status = true,
+		enable_diagnostics = true,
+		default_component_configs = {
+			indent = {
+				indent_size = 2,
+				with_markers = true,
+				indent_marker = "│",
+				last_indent_marker = "└",
+				highlight = "NeoTreeIndentMarker",
+				with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
+				expander_collapsed = "",
+				expander_expanded = "",
+				expander_highlight = "NeoTreeExpander",
+			},
+			icon = {
+				folder_closed = "",
+				folder_open = "",
+				folder_empty = "󰜌",
+			},
+		},
+		filesystem = {
+			filtered_items = {
+				hide_dotfiles = false,
+				hide_gitignored = false,
+				hide_hidden = false,
+			},
+		},
+		vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left<CR>", {}),
+		vim.keymap.set("n", "<leader>w", ":Neotree filesystem close<CR>", {}),
+	},
 }
