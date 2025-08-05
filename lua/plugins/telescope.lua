7b0819a4bdeb3722c7d72fc074e8af373dc8c1ab
Fabio Perez <fanpero@mac.home>
2025-08-05 00:44:02 -0400

    changes to telescope plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,27 +1,32 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		branch = "0.1.x",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>fs", builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-			vim.keymap.set("n", "<leader>fo", builtin.oldfiles, {})
-		end,
-	},
-	{
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-tree/nvim-web-devicons",
 		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
 	},
+	config = function()
+		local telescope = require("telescope")
+
+		telescope.setup({
+			defaults = {
+				path_display = { "smart" },
+			},
+			extensions = {
+				["ui-select"] = {
+					require("telescope.themes").get_dropdown({}),
+				},
+			},
+		})
+
+		telescope.load_extension("ui-select")
+		telescope.load_extension("fzf")
+
+		vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
+		vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in files" })
+		vim.keymap.set("n", "<leader>fo", "<cmd>Telescope oldfiles<cr>", { desc = "Find recent files" })
+		vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Open buffers" })
+	end,
 }
