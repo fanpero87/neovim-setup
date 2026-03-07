ca29d6c80c6f1c148a160391d282f92b1060e2a6
Fabio Perez <fabio@fongo.com>
2026-03-06 23:55:27 -0500

    finall change

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,16 +1,12 @@
 return {
 	"nvimtools/none-ls.nvim",
-  dependencies = { "nvimtools/none-ls-extras.nvim" },
 	config = function()
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.php_cs_fixer,  -- Alternative for non-Laravel PHP
         null_ls.builtins.formatting.shfmt,          -- bash/sh formatter
-        null_ls.builtins.diagnostics.shellcheck,    -- bash static analysis
-        require("none-ls.formatting.blade_formatter"), -- from extras
 			},
 		})
 
